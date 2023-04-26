688c394cc3df6e67238c0aaf1e9e6bf702c64a05
giorgi kiladze <giorgikiladze92@gmail.com>
2023-04-26 01:37:58 -0700

    feat:add checkbox functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import './App.css'
+import {useState} from "react";
 import CopyImg from"./assets/images/icon-copy.svg"; 
 import Check from "./assets/images/icon-check.svg";
 import Arrow from "./assets/images/icon-arrow-right.svg";
 
 function App() {
+           
+    const [inputValue, setInputValue] = useState(0);
+    const [uppercaseCheck, setUppercaseCheck] = useState(false);
+    const [lowercaseCheck, setLowercaseCheck] = useState(false);
+    const [numbers, setNumbers] = useState(false)
+    const [symbols, setSymbols] = useState(false);
+
+
+    const symbolsFunction = () => {
+        setSymbols(!symbols)
+    };
+
+    const numbersFunction = () => {
+        setNumbers(!numbers)
+    };
+
+    const lowercaseFunction = () => {
+        setLowercaseCheck(!lowercaseCheck);
+    };
+
+    const uppercaseFunction = () => {
+        setUppercaseCheck(!uppercaseCheck)
+    };
+
+    const handleInputChange = (event) => {
+      setInputValue(event.target.value)
+    };
+
 
     
+  
 
  return ( <div>
            <p className='title'>Password Generator</p>
@@ -17,33 +47,41 @@ function App() {
            <div className='Card'>
                  <div className='titleContainer'>
                    <p className='cardTitle'>Character Length</p>
-                   <p className='characterLength'>10</p>
+                   <p className='characterLength'>{inputValue}</p>
                  </div>
               
             <div className="sliderContainer">
-                 <div className="slideBackground"></div>
-                 <input className="slider" type="range" min="0" max="20" value="0" id="ranger"/>
+                 <div className="slideBackground" > </div>
+                 <input className="slider" type="range" min="0" max="20" value={inputValue} id="ranger"  onChange={handleInputChange}/>
            </div>
 
               
                <div className='checkboxContainer first'>
-                   <div className='firstCheckBox'></div>
+                   <div className='firstCheckBox' onClick={ uppercaseFunction} style={{background:uppercaseCheck?"#A4FFAF":"transparent"}} >
+                    {uppercaseCheck? <img className='FirstCheck' src={Check} alt="" />:null}
+                    </div>
                    <p className='checkboxText'>Include Uppercase Letters</p>
                </div>  
 
                <div className='checkboxContainer'>
-                   <div className='secondCheckBox'></div>
+                   <div className='secondCheckBox' onClick={lowercaseFunction} style={{background:lowercaseCheck?"#A4FFAF":"transparent"}}>
+                    {lowercaseCheck?<img className='secondCheck' src={Check} alt="" />:null }
+                   </div>
                    <p className='checkboxText'>Include Lowercase Letters</p>
                </div>
                   
                <div className='checkboxContainer'>
-                   <div className='thirdCheckBox'></div>
+                   <div className='thirdCheckBox' onClick={numbersFunction} style={{background:numbers?"#A4FFAF":"transparent"}}>
+                    {numbers?<img className='thirdCheck' src={Check} alt="" />:null}
+                   </div>
                    <p className='checkboxText'>Include Numbers</p>
                </div>
 
 
                <div className='checkboxContainer'>
-                   <div className='fourthCheckBox'></div>
+                   <div className='fourthCheckBox' onClick={symbolsFunction} style={{background:symbols?"#A4FFAF":"transparent"}}>
+                    {symbols?<img className='fourthCheckbox' src={Check} alt="" />:null}
+                   </div>
                    <p className='checkboxText'>Include Symbols</p>
                </div>
 
